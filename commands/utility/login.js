418f2ae63f13ecf6cd29bf8994c12564faf44295
Aloken Chaudhari <alokench@gmail.com>
2024-05-15 13:51:10 -0600

    added a reload command, event listener, and added a randomized greeting to \login

--- commands/utility/login.js
+++ commands/utility/login.js
@@ -2,11 +2,14 @@
 
 const { SlashCommandBuilder } = require("discord.js");
 
+const myArray = ['Howdy', 'Good Day', 'What up dawg', 'Hello', 'Hi', 'Heya'];  
+const randomElement = myArray[Math.floor(Math.random() * myArray.length)];  
+
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('login')
         .setDescription('User recieves points for using this command once a day'),
     async execute(interaction){
-        await interaction.reply({content: '{randomized welcome}! You have recieved X points for logging in today!', ephemeral: true});
+        await interaction.reply({content: `${randomElement}! You have recieved X points for logging in today!`, ephemeral: true});
     },
 };
